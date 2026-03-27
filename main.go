f99932d5beafd4696cef8a3fa40a54fbe1172a64
Madik981 <madi12312456@gmail.com>
2026-03-27 21:48:36 +0500

    Added project starter code

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"Book-API-Gin_Golang/handlers"
+	"Book-API-Gin_Golang/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	store := models.NewStore()
+
+	handlers.RegisterRoutes(r, store)
+
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
