c30be112e97904872bb0a3534ce200b97698c9f6
Madik981 <madi12312456@gmail.com>
2026-04-11 08:59:58 +0500

    Enhanced model definitions with GORM tags

--- models/author.go
+++ models/author.go
@@ -1,8 +1,8 @@
 package models
 
 type Author struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID   int    `json:"id" gorm:"primaryKey;autoIncrement"`
+	Name string `json:"name" gorm:"size:255;not null"`
 }
 
 type CreateAuthorInput struct {
--- models/book.go
+++ models/book.go
@@ -1,11 +1,13 @@
 package models
 
 type Book struct {
-	ID         int     `json:"id"`
-	Title      string  `json:"title"`
-	AuthorID   int     `json:"author_id"`
-	CategoryID int     `json:"category_id"`
-	Price      float64 `json:"price"`
+	ID         int      `json:"id" gorm:"primaryKey;autoIncrement"`
+	Title      string   `json:"title" gorm:"size:255;not null"`
+	AuthorID   int      `json:"author_id" gorm:"not null;index"`
+	CategoryID int      `json:"category_id" gorm:"not null;index"`
+	Price      float64  `json:"price" gorm:"not null"`
+	Author     Author   `json:"-" gorm:"foreignKey:AuthorID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
+	Category   Category `json:"-" gorm:"foreignKey:CategoryID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
 }
 
 type CreateBookInput struct {
