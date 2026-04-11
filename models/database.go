1cb182e3c22f5cd0dd947ca17c69382200475e29
Madik981 <madi12312456@gmail.com>
2026-04-11 09:00:52 +0500

    Implemented DB connection and model migrations

---
+++ models/database.go
@@ -0,0 +1,52 @@
+package models
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewPostgresDBFromEnv() (*gorm.DB, error) {
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		host := getEnv("DB_HOST", "localhost")
+		port := getEnv("DB_PORT", "5432")
+		user := getEnv("DB_USER", "postgres")
+		password := getEnv("DB_PASSWORD", "postgres")
+		dbName := getEnv("DB_NAME", "book_api")
+		sslMode := getEnv("DB_SSLMODE", "disable")
+		timeZone := getEnv("DB_TIMEZONE", "UTC")
+
+		dsn = fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+			host,
+			user,
+			password,
+			dbName,
+			port,
+			sslMode,
+			timeZone,
+		)
+	}
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.AutoMigrate(&Author{}, &Category{}, &Book{}, &User{}); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func getEnv(key string, fallback string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return fallback
+	}
+	return value
+}
