eb421cdccacdee2a3641ff645af89e9bb2310a4b
Madik981 <madi12312456@gmail.com>
2026-04-11 08:45:44 +0500

    Refactored routes. Include Auth endpoints, protect routes with JWT middleware

--- handlers/routes.go
+++ handlers/routes.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"Book-API-Gin_Golang/models"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,16 +11,35 @@ func RegisterRoutes(r *gin.Engine, store *models.Store) {
 	bookHandler := NewBookHandler(store)
 	authorHandler := NewAuthorHandler(store)
 	categoryHandler := NewCategoryHandler(store)
+	jwtSecret := resolveJWTSecret()
+	authHandler := NewAuthHandler(store, jwtSecret)
+
+	r.POST("/auth/register", authHandler.Register)
+	r.POST("/auth/login", authHandler.Login)
 
 	r.GET("/books", bookHandler.ListBooks)
-	r.POST("/books", bookHandler.CreateBook)
 	r.GET("/books/:id", bookHandler.GetBookByID)
-	r.PUT("/books/:id", bookHandler.UpdateBook)
-	r.DELETE("/books/:id", bookHandler.DeleteBook)
 
 	r.GET("/authors", authorHandler.ListAuthors)
-	r.POST("/authors", authorHandler.CreateAuthor)
 
 	r.GET("/categories", categoryHandler.ListCategories)
-	r.POST("/categories", categoryHandler.CreateCategory)
+
+	protected := r.Group("/")
+	protected.Use(AuthMiddleware(jwtSecret))
+	{
+		protected.POST("/books", bookHandler.CreateBook)
+		protected.PUT("/books/:id", bookHandler.UpdateBook)
+		protected.DELETE("/books/:id", bookHandler.DeleteBook)
+
+		protected.POST("/authors", authorHandler.CreateAuthor)
+		protected.POST("/categories", categoryHandler.CreateCategory)
+	}
+}
+
+func resolveJWTSecret() []byte {
+	secret := os.Getenv("JWT_SECRET")
+	if secret == "" {
+		secret = "dev-secret-change-me"
+	}
+	return []byte(secret)
 }
